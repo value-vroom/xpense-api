024881d92aa2711357ae0f8467c52d1405831de3
Mathias Gredal <matg@universal-robots.com>
2023-11-26 19:45:32 +0100

    Allow edit user profile

--- app/routers/login.py
+++ app/routers/login.py
@@ -3,9 +3,8 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Form, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from prisma.models import Token
-
-from app.utility.security import authenticate_user, create_access_token, get_password_hash
+from prisma.models import Token, User
+from app.utility.security import authenticate_user, create_access_token, get_password_hash, get_current_user
 from prisma import get_client
 
 router = APIRouter(tags=["Login"])
@@ -53,3 +52,33 @@ def signup(
         access_token=access_token,
         token_type="bearer",  # noqa: S106
     )
+
+
+# Support updating the user's profile data
+@router.put("/users/{username}")
+def update_user(  # noqa: PLR0913
+    username: str,
+    email: Annotated[str, Form()],
+    full_name: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    profile_image: Annotated[str, Form()],
+    current_user: Annotated[User, Depends(get_current_user)],
+) -> User:
+    """Update a user's profile data"""
+    db = get_client()
+
+    # Check if the current user is the same as the user being updated
+    if current_user.username != username:
+        raise Exception("You can only update your own profile")
+
+    db.user.update(
+        where={"username": username},
+        data={
+            "email": email,
+            "full_name": full_name,
+            "hashed_password": get_password_hash(password),
+            "profile_image": profile_image,
+        },
+    )
+
+    return db.user.find_first(where={"username": username})
